5945410099419b75bd54bb36619ff528d8e8a21d
Alexander Savchenko <sasha.savchenko@gmail.com>
2019-06-24 01:58:48 +0300

    Homework 9 finished

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-alert('Helloworld');
+
+const setActive = event => {
+    if (event.target.className !== 'tabs-title active') {
+    let current = document.getElementsByClassName('tabs-title active')[0];
+        let curLi = document.querySelector(`[data-id=${current.innerText}`);
+        curLi.dataset.show = "no";
+        current.className = 'tabs-title';
+    event.target.className = 'tabs-title active';
+        let li = document.querySelector(`[data-id=${event.target.innerText}]`);
+        li.dataset.show = "yes";
+    } else return;
+}
+
+document.querySelectorAll(".tabs-title").forEach(el => {
+    el.addEventListener("click", setActive);
+})
